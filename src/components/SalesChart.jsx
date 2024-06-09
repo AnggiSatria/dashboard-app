d2ccd14407f5021e991fb99a632813d4f44c5f1a
AnggiSatria <anggi.satria@talentinsider.com>
2024-06-09 21:43:16 +0700

    [Anggi]: section revision fetch dashboard sales and statistic

--- src/components/SalesChart.jsx
+++ src/components/SalesChart.jsx
@@ -3,18 +3,19 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { readAllSales } from "@/utils/hooks";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -31,42 +32,32 @@ export default function SalesChart(props) {
       },
       title: {
         display: false,
-        text: "Chart.js Bar Chart",
+        text: "Chart.js Line Chart",
       },
     },
   };
 
-  const labels = dataSales?.map((res) => {
-    return res?.product;
-  });
+  const labels = dataSales?.map((item) => item.product);
+  const sales = dataSales?.map((item) => item.sales);
+  const revenue = dataSales?.map((item) => item.revenue);
 
   const data = {
     labels,
     datasets: [
       {
-        label: "Dataset 1",
-        data: [120, 250, 300, 500, 450, 320, 540],
+        label: "Sales",
+        data: sales,
+        borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
-        label: "Dataset 2",
-        data: [200, 400, 350, 600, 500, 450, 600],
+        label: "Revenue",
+        data: revenue,
+        borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
   };
-
-  useEffect(() => {
-    data.datasets.push(
-      dataSales?.map((res) => {
-        return {
-          label: res?.product,
-          data: [120, 250, 300, 500, 450, 320, 540],
-          backgroundColor: "rgba(255, 99, 132, 0.5)",
-        };
-      })
-    );
-  }, []);
 
   return (
     <>
@@ -76,7 +67,8 @@ export default function SalesChart(props) {
             Sales
           </div>
           <div className="flex w-full flex-grow md:h-[350px]">
-            <Bar options={options} data={data} className="!w-full !h-full" />
+            {/* <Bar options={options} data={data} className="!w-full !h-full" /> */}
+            <Line options={options} data={data} className="!w-full !h-full" />
           </div>
         </div>
       </div>
--- src/components/Statictics.jsx
+++ src/components/Statictics.jsx
@@ -1,38 +1,101 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import React, { useEffect } from "react";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const data = {
+//   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//   datasets: [
+//     {
+//       label: "# of Votes",
+//       data: [12, 19, 3, 5, 2, 3],
+//       backgroundColor: [
+//         "rgba(255, 99, 132, 0.2)",
+//         "rgba(54, 162, 235, 0.2)",
+//         "rgba(255, 206, 86, 0.2)",
+//         "rgba(75, 192, 192, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(255, 159, 64, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(255, 99, 132, 1)",
+//         "rgba(54, 162, 235, 1)",
+//         "rgba(255, 206, 86, 1)",
+//         "rgba(75, 192, 192, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(255, 159, 64, 1)",
+//       ],
+//       borderWidth: 1,
+//     },
+//   ],
+// };
 
 export default function Statictics(props) {
   const { dataSales, isLoading } = props;
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  const labels = dataSales?.map((item) => item.product);
+  const sales = dataSales?.map((item) => item.sales);
+  const revenue = dataSales?.map((item) => item.revenue);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Sales",
+        data: sales,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Revenue",
+        data: revenue,
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  useEffect(() => {
+    data.datasets.push(
+      dataSales?.map((res) => {
+        return {
+          label: res?.product,
+          data: [120, 250, 300, 500, 450, 320, 540],
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+        };
+      })
+    );
+  }, []);
 
   return (
     <>
@@ -42,7 +105,7 @@ export default function Statictics(props) {
             Statistics
           </div>
           <div className="flex w-full flex-grow md:h-[350px] justify-center">
-            <Doughnut data={data} className="" />
+            <Bar options={options} data={data} className="!w-full !h-full" />
           </div>
         </div>
       </div>
